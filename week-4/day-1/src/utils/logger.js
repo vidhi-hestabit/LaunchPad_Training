6cf4d03f8b3381070d357fb5f674dfa5225666bb
vidhi-hestabit <vidhia.hestabit@gmail.com>
2025-11-25 17:28:35 +0530

    Utils logger file added

---
+++ week-4/day-1/src/utils/logger.js
@@ -0,0 +1,15 @@
+import winston, { log } from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: "HH:mm:ss" }),
+    winston.format.printf(
+      (info) => `[${info.timestamp}] ${info.level}: ${info.message}`
+    )
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
