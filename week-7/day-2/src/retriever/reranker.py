d213185c552dfa2a9b2247cd5851538ca5266093
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-02 17:01:49 +0530

    reranker added

--- week-7/day-2/src/retriever/reranker.py
+++ week-7/day-2/src/retriever/reranker.py
@@ -1,18 +1,15 @@
 from sentence_transformers import CrossEncoder
 
-
 class Reranker:
-    def __init__(self, model_name="cross-encoder/ms-marco-MiniLM-L-6-v2"):
-        self.model = CrossEncoder(model_name)
+    def __init__(self, model="cross-encoder/ms-marco-MiniLM-L-6-v2"):
+        self.model = CrossEncoder(model)
 
-    def rerank(self, query, chunks, top_k=5):
-        pairs = [(query, chunk["text"]) for chunk in chunks]
+    def rerank(self, query, docs, top_k=5):
+        pairs = [(query, d["text"]) for d in docs]
         scores = self.model.predict(pairs)
-
         ranked = sorted(
-            zip(chunks, scores),
+            zip(docs, scores),
             key=lambda x: x[1],
             reverse=True
         )
-
-        return [chunk for chunk, _ in ranked[:top_k]]
+        return [d for d, _ in ranked[:top_k]]
