66a8928de19fe92495e1943b26466447573ef8a9
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-05 15:35:37 +0530

    Removed day-3

--- week-7/day-3-final/src/utils/ocr.py
+++
@@ -1,5 +0,0 @@
-import pytesseract
-from PIL import Image
-
-def extract_ocr_text(image: Image.Image) -> str:
-    return pytesseract.image_to_string(image).strip()
--- week-7/day-3-final/src/utils/pdf_utils.py
+++
@@ -1,5 +0,0 @@
-from pdf2image import convert_from_path
-
-def pdf_to_images(pdf_path: str):
-    images = convert_from_path(pdf_path, dpi=300)
-    return images
