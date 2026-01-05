b2d8991aeabe246ba25c1f3bd5cc155d9212cecc
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-05 15:34:23 +0530

    Removed day-2

--- week-7/day-2/RETRIEVAL_STRATEGIES.md
+++
@@ -1,8 +0,0 @@
-
-Architecture for the Online tasks (performed at Run-time) -
-
-![alt text](image.png)
-
-Architecture for the Offline tasks -
-
-![alt text](image-1.png)
--- week-7/day-2/src/retriever/hybrid_retriever.py
+++
@@ -1,8 +0,0 @@
-class HybridRetriever:
-    def __init__(self, vector_db, embedder):
-        self.vector_db = vector_db
-        self.embedder = embedder
-
-    def retrieve(self, query, top_k=10):
-        query_emb = self.embedder.embed([query])[0]
-        return self.vector_db.search(query_emb, top_k)
