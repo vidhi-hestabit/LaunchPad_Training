08104450aac7f2464678a880df1c4905f18bc27d
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-23 15:53:23 +0530

    Added model_client - work as llm_client

---
+++ week-9/day-3/utils/model_client.py
@@ -0,0 +1,18 @@
+import os
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+def get_model_client():
+    return OpenAIChatCompletionClient(
+        model="llama-3.1-8b-instant",
+        api_key=os.getenv("GROQ_API_KEY"),
+        base_url="https://api.groq.com/openai/v1",
+        temperature=0.7,
+        max_tokens=2048,
+        model_info={
+            "type": "openai",
+            "json_output": False,
+            "vision": False,
+            "function_calling": False,
+            "family":"llama"
+        }
+    )
