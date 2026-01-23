3a027a8f6535e293b097f9ab04663c766c2656df
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-23 19:36:36 +0530

    Day-2 complete

--- week-9/day-2/utils/model_client.py
+++ week-9/day-2/utils/model_client.py
@@ -1,12 +1,15 @@
 import os
 from autogen_ext.models.openai import OpenAIChatCompletionClient
+from dotenv import load_dotenv
+
+load_dotenv() 
+
 
 def get_model_client():
     return OpenAIChatCompletionClient(
         model="llama-3.3-70b-versatile",
         api_key=os.getenv(
-            "GROQ_API_KEY",
-            "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"
+            "OPENAI_API_KEY"
         ),
         base_url="https://api.groq.com/openai/v1",
         temperature=0.2,
