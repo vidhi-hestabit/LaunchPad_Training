b9aba70b25e719df25fb73f19556d4dabb7c88e2
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-01-31 00:41:18 +0530

    Agent run call

---
+++ week-9/day-2-new/utils/agent_runner.py
@@ -0,0 +1,10 @@
+from autogen_agentchat.messages import TextMessage
+from autogen_core import CancellationToken
+
+async def run_agent(agent, prompt):
+    cancellation_token = CancellationToken()
+    response = await agent.on_messages(
+        [TextMessage(content=prompt, source="user")],
+        cancellation_token,
+    )
+    return response.chat_message.content
